45473064fd0551a8450fcb94a251ea7656ccdbfa
Jeareth Venegas <venegasjea@gmail.com>
2021-01-29 11:23:09 -0600

    practica dom

---
+++ script.js
@@ -0,0 +1,32 @@
+/**
+ * Ejercicio #4:
+ *
+ * Validar el formulario
+ *
+ * ¿Como funciona?:
+ * 1. La validacion se hace cuando el usuario presiona el boton "Enviar" para enviar el forom
+ * 2. Solo se validan los campos con el * (tienen la clase requerido)
+ * 3. Al hacer click en "enviar"
+ *     3.1. Los campos invalidos deben ponerse con un borde rojo
+ *     3.2. Se debe mostrar un aviso con un titulo de: "Ocurrio un error, verifica
+ *     los siguientes campos" y seguido un listado con los campos invalidos. El aviso
+ *     debe tener border rojo y fondo rojo claro
+ *     3.3. Si todos los camppos son validos, debe mostrar un mensaje abajo del boton enviar
+ *     que dice: "Su formulario fue enviado". El aviso debe tener borde verde con fondo verde claro
+ *
+ * ¿Donde empiezo?:
+ *
+ * ¿Que voy a evaluar?
+ * 1. Que funcione
+ * 2. Buenas practicas (incluyendo buenas practicas pasadas)
+ *
+ */
+
+// BOILERPLATE
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  console.log("FORMULARIO ENVIADO");
+});
